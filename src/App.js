ee1931c09ac549e1c3c5822570d01745d38ab923
JeanSilva08 <jeancarlossoares.s@gmail.com>
2023-07-27 13:01:45 -0300

    AddAlbumForm feature

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
 import React, { useState } from 'react';
 import NavBar from './components/NavBar';
 import SearchBar from './components/SearchBar';
+import AddAlbumForm from './components/AddAlbumForm';
 
 const App = () => {
-  // Simule uma lista de álbuns para teste
   const [albums, setAlbums] = useState([
     { id: 1, name: 'Álbum 1' },
     { id: 2, name: 'Álbum 2' },
     { id: 3, name: 'Álbum 3' },
-    // Adicione mais álbuns aqui para teste
   ]);
 
   const handleSearch = (keyword) => {
-    // Filtrar a lista de álbuns com base na keyword
     const filteredAlbums = albums.filter((album) =>
       album.name.toLowerCase().includes(keyword.toLowerCase())
     );
-    // Atualizar a lista de álbuns com os álbuns filtrados
     setAlbums(filteredAlbums);
+  };
+
+  const handleAddAlbum = (albumName) => {
+    const newAlbum = {
+      id: albums.length + 1,
+      name: albumName,
+    };
+    setAlbums([...albums, newAlbum]);
   };
 
   return (
     <div className="App">
       <NavBar />
-      <SearchBar onSearch={handleSearch} /> {/* Passar a função de filtro */}
+      <SearchBar onSearch={handleSearch} />
+      <AddAlbumForm onAddAlbum={handleAddAlbum} /> {/* Novo componente para adicionar álbuns */}
       <div className="album-list">
         <h2>Lista de Álbuns:</h2>
         <ul>
---
+++ src/components/AddAlbumForm.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+const AddAlbumForm = ({ onAddAlbum }) => {
+  const [albumName, setAlbumName] = useState('');
+
+  const handleChange = (event) => {
+    setAlbumName(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (albumName.trim() !== '') {
+      onAddAlbum(albumName);
+      setAlbumName(''); 
+    }
+  };
+
+  return (
+    <form className="add-album-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Digite o nome do novo álbum..."
+        value={albumName}
+        onChange={handleChange}
+      />
+      <button type="submit">Adicionar</button>
+    </form>
+  );
+};
+
+export default AddAlbumForm;
